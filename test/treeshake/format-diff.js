b89a1add67e85599d5c0f232470f8e8c823aaf7d
Marco Fugaro <marco.fugaro@gmail.com>
2023-03-02 19:48:46 +0100

    more accurate

--- test/treeshake/format-diff.js
+++ test/treeshake/format-diff.js
@@ -4,6 +4,7 @@ const filesize = Number( process.argv[ 2 ] );
 const filesizeBase = Number( process.argv[ 3 ] );
 
 const diff = ( filesize - filesizeBase ) * 100 / filesizeBase;
-const formatted = `${diff >= 0 ? '+' : ''}${diff.toFixed( 1 )}%`;
+const diffString = diff.toFixed( 2 ).slice( - 1 ) === '0' ? diff.toFixed( 1 ) : diff.toFixed( 2 );
+const formatted = `${diff >= 0 ? '+' : ''}${diffString}%`;
 
 console.log( formatted );
